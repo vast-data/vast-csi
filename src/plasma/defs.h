689ed217d6e215fcd0bf1cbe5d055cfe4aaed1c4
Alon Horev <alon@vastdata.com>
2016-03-28 10:41:37 +0300

    Update copyright

--- src/plasma/defs.h
+++ src/plasma/defs.h
@@ -1,7 +1,4 @@
-/* Copyright (C) Vast Data, Inc - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly
- * prohibited proprietary and confidential.
- */
+/* Copyright (C) Vast Data Ltd. */
 
 /*!
  * \file defs.h
