74080ea689a82c226a087463fdb22491d48816eb
Alon Horev <alon@vastdata.com>
2016-08-23 14:01:16 +0300

    Improve installation script of gprof2dot

--- scripts/install_gprof2dot.sh
+++ scripts/install_gprof2dot.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+sudo yum -y install graphviz
+
 curl https://bootstrap.pypa.io/get-pip.py | sudo python2
 sudo pip2.7 install setuptools virtualenv
 
 virtualenv venv2
 venv2/bin/pip install gprof2dot
 
-# how to use: ./dist/tests/nfs && gprof ./dist/tests/nfs | ./venv2/bin/gprof2dot | dot -Tpng -o output.png
+echo "Usage: ./dist/tests/nfs && gprof ./dist/tests/nfs | ./venv2/bin/gprof2dot | dot -Tpng -o output.png"
