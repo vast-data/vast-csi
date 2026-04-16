08a6036c19999254161f075789c92a5295c13434
Elyasaf Boim <elyasaf.boim@vastdata.com>
2026-04-16 14:48:03 +0300

    Revert "Merge branch 'mtls' into v2.6"
    This reverts commit 3eaab39e7a6b68521ac7c7db25af708aec27e727, reversing
    changes made to 8c91ea157ecfcd63068b14eacf7efd2a85b953bd.

--- vast_csi/plugins/base.py
+++ vast_csi/plugins/base.py
@@ -41,7 +41,7 @@ from vast_csi.exceptions import Abort, LookupFieldError
 from vast_csi.session import instantiate_session_from_secret, VmsSession
 from vast_csi.luks_utils import get_luks_manager, LuksManager
 from vast_csi.metrics import get_metrics_registry
-from vast_csi.mtls_utils import get_mtls_manager
+from vast_csi.mtls_utils import get_mtls_manager, MtlsManager
 from vast_csi.quantity import parse_quantity
 from vast_csi.logging import logger
 
@@ -527,20 +527,26 @@ class NodeBase(csi_grpc.NodeServicer):
             is_ephemeral,
             vms_session,
             luks_manager=None,
+            mtls_manager=None,
     ):
         """
         Stores metadata about a volume in a file, including information about
-        whether the volume is ephemeral, host_encryption and, if so, serialized session data.
+        whether the volume is ephemeral, host_encryption, mTLS and, if so, serialized session data.
         """
         payload = {
         "volume_id": volume_id,
         "is_ephemeral": is_ephemeral,
         }
 
+        if mtls_manager and mtls_manager.xprtsec:
+            payload["xprtsec"] = mtls_manager.xprtsec
+
         if is_ephemeral:
             payload["vms_session"] = vms_session.serialize(salt=volume_id)
             if luks_manager:
                 payload["luks_manager"] = luks_manager.serialize(salt=volume_id)
+            if mtls_manager:
+                payload["mtls_manager"] = mtls_manager.serialize(salt=volume_id)
 
         with meta_file.open("w") as f:
             json.dump(payload, f)
@@ -574,5 +580,8 @@ class NodeBase(csi_grpc.NodeServicer):
             self.controller.DeleteVolume.__wrapped__(
                 self.controller, vms_session=vms_session, volume_id=meta["volume_id"]
             )
+
+        if meta.get("xprtsec") == "mtls":
+            MtlsManager.delete_credentials(volume_id)
 
         return meta
